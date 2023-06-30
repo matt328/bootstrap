e252b85b786e7d57a92cb2b79c00780738dd60e1
Matt <matt328@users.noreply.github.com>
2023-06-29 21:48:03 -0400

    Missing piece of argo managing itself finally clicked

--- argocd/argocd.tf
+++ argocd/argocd.tf
@@ -8,21 +8,23 @@ terraform {
 }
 
 provider "kustomization" {
-  kubeconfig_path = "~/.kube/neon-config"
+  // kubeconfig_path = "~/.kube/neon-config"
+  kubeconfig_path = "C:/Users/Matt/.kube/neon-config"
 }
 
 module "argocd_install" {
   source  = "kbst.xyz/catalog/argo-cd/kustomization"
-  version = "v2.6.7-kbst.0"
+  version = "2.6.7-kbst.0"
 
   configuration_base_key = "default"
 
   configuration = {
     default = {
+      additional_resources = ["${path.root}/manifests/traefik-ingress.yml"]
       config_map_generator = [{
-        name = "argocd-cmd-params-cm"
+        name      = "argocd-cmd-params-cm"
         namespace = "argocd"
-        behavior = "merge"
+        behavior  = "merge"
         literals = [
           "server.insecure=true"
         ]
@@ -31,3 +33,7 @@ module "argocd_install" {
     }
   }
 }
+
+// After doing this, you have to kubectl apply something like this https://github.com/JuanWigg/self-managed-argo/blob/main/applications.yaml
+// which defines (a potentially different version of) argocd to be managed by itself from now on.  The intially installed instance of argo
+// is almost a temporary bootstrapper that might get overwritten by itself.
