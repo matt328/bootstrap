dd3c55bcbbf9325d97056f37b8fa0b47ea039302
Matt Teeter <matt328@gmail.com>
2025-11-03 16:09:17 -0500

    Add separate cluster files

---
+++ provisioning/clusters/citadel.rb
@@ -0,0 +1,10 @@
+BRIDGE = "Realtek PCIe GbE Family Controller"
+DNS_SERVER = "192.168.50.210"
+GITHUB_USER = "matt328"
+
+NODES = [
+  { name: "citadel", ip: "192.168.50.120", role: "master" },
+  { name: "zakera",  ip: "192.168.50.121", role: "agent"  },
+  { name: "kithoi",  ip: "192.168.50.122", role: "agent"  },
+  { name: "shalta",  ip: "192.168.50.123", role: "agent"  },
+]
---
+++ provisioning/clusters/ilium.rb
@@ -0,0 +1,10 @@
+BRIDGE = "Intel(R) Ethernet Connection (22) I219-LM"
+DNS_SERVER = "192.168.50.210"
+GITHUB_USER = "matt328"
+
+NODES = [
+  { name: "ilium", ip: "192.168.50.100", role: "master" },
+  { name: "horizon",  ip: "192.168.50.101", role: "agent"  },
+  { name: "rannoch",  ip: "192.168.50.102", role: "agent"  },
+  { name: "noveria",  ip: "192.168.50.103", role: "agent"  },
+]
