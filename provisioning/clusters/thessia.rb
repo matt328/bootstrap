14919780a4f275e8bbd8876bfeee4f4399e62a49
Matt Teeter <matt328@gmail.com>
2025-11-03 16:15:23 -0500

    Fixed cluster definitions

--- provisioning/clusters/thessia.rb
+++ provisioning/clusters/thessia.rb
@@ -1,10 +1,10 @@
-BRIDGE = "Intel(R) Ethernet Connection (22) I219-LM"
+BRIDGE = "Realtek PCIe GbE Family Controller"
 DNS_SERVER = "192.168.50.210"
 GITHUB_USER = "matt328"
 
 NODES = [
-  { name: "ilium", ip: "192.168.50.100", role: "master" },
-  { name: "horizon",  ip: "192.168.50.101", role: "agent"  },
-  { name: "rannoch",  ip: "192.168.50.102", role: "agent"  },
-  { name: "noveria",  ip: "192.168.50.103", role: "agent"  },
+  { name: "thessia", ip: "192.168.50.110", role: "master" },
+  { name: "feros",  ip: "192.168.50.111", role: "agent"  },
+  { name: "therum",  ip: "192.168.50.112", role: "agent"  },
+  { name: "virmire",  ip: "192.168.50.113", role: "agent"  },
 ]
