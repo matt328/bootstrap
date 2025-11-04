52a4dd8e20c43040a0f83787d037566d7d439be3
Matt Teeter <matt328@gmail.com>
2025-11-04 11:26:15 -0500

    Fix networking issue

--- provisioning/clusters/illium.rb
+++ provisioning/clusters/illium.rb
@@ -3,7 +3,7 @@ DNS_SERVER = "192.168.50.210"
 GITHUB_USER = "matt328"
 
 NODES = [
-  { name: "ilium", ip: "192.168.50.100", role: "master" },
+  { name: "illium", ip: "192.168.50.100", role: "master" },
   { name: "horizon",  ip: "192.168.50.101", role: "agent"  },
   { name: "rannoch",  ip: "192.168.50.102", role: "agent"  },
   { name: "noveria",  ip: "192.168.50.103", role: "agent"  },
