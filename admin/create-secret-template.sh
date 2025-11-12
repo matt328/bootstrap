74749882cf37cd2523a5f37f63302843af1c6646
Matt Teeter <matt328@gmail.com>
2025-11-12 15:04:03 -0500

    Update for incremental deployment

--- admin/create-secret-template.sh
+++ admin/create-secret-template.sh
@@ -2,7 +2,7 @@ kubectl create secret generic test-secret \
 --from-literal=type=git \
 --from-literal=url=git@github.com:matt328/bootstrap \
 --from-literal=username=matt328 \
---from-literal=password= \
+--from-literal=password=pw \
 --namespace=management \
 --dry-run=client \
 -o yaml | kubeseal --format yaml > test-sealed-secret.yaml
